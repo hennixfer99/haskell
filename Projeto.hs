e4532ea0dd3c0e000ea1ad16476af9d6b7bd3918
Henrique Ferreira <Henriferreira99@gmail.com>
2026-08-18 14:51:09 -0300

    Ainda tem erro, jaja arrumo

--- Projeto.hs
+++ Projeto.hs
@@ -1,20 +1,69 @@
 module Projeto where
 
-data Cargo = Estagiario | Programador | Coordenador | Gerente deriving Show
+-- data Cargo = Estagiario | Programador | Coordenador | Gerente deriving Show
 
-data Pessoa = Pessoa {cargo :: Cargo, nome :: String} deriving Show
+-- data Pessoa = Pessoa {cargo :: Cargo, nome :: String} deriving Show
 
-verSalario :: Pessoa -> Double
-verSalario (Pessoa Estagiario _) = 1500
-verSalario (Pessoa Programador _) = 5750.15
-verSalario (Pessoa Coordenador _) = 8000
-verSalario (Pessoa Gerente _) = 10807.20
+-- verSalario :: Pessoa -> Double
+-- verSalario (Pessoa Estagiario _) = 1500
+-- verSalario (Pessoa Programador _) = 5750.15
+-- verSalario (Pessoa Coordenador _) = 8000
+-- verSalario (Pessoa Gerente _) = 10807.20
 
-verFolha :: Pessoa -> String
-verFolha p = "{nome: \"" ++ (nome p) ++ "\", cargo: \"" ++ show (cargo p) ++ "\", salario: " ++ show (verSalario p) ++ "}"
+-- verFolha :: Pessoa -> String
+-- verFolha p = "{nome: \"" ++ (nome p) ++ "\", cargo: \"" ++ show (cargo p) ++ "\", salario: " ++ show (verSalario p) ++ "}"
 
-promover :: Pessoa -> Pessoa
-promover (Pessoa Estagiario n) = Pessoa Programador n
-promover (Pessoa Programador n) = Pessoa Coordenador n
-promover (Pessoa _ n) = Pessoa Gerente n
+-- promover :: Pessoa -> Pessoa
+-- promover (Pessoa Estagiario n) = Pessoa Programador n
+-- promover (Pessoa Programador n) = Pessoa Coordenador n
+-- promover (Pessoa _ n) = Pessoa Gerente n
 
+-- module ExDoLivro where
+
+data Pergunta = Sim | Nao deriving Show
+
+pergNum :: Pergunta -> Int
+pergNum Sim = 1
+pergNum Nao = 0
+
+-- pergNum _ = error "pergNum: argumento invalido"
+
+perguntas :: [Pergunta] -> [Int]
+perguntas x = map pergNum x
+
+and' :: Pergunta -> Pergunta -> Int
+and' Sim Sim = pergNum Sim
+and' Nao Nao = pergNum Nao
+and' _ _ = pergNum Nao
+
+or' :: Pergunta -> Pergunta -> Int
+or' Sim _ = pergNum Sim
+or' _ Sim = pergNum Sim
+or' _ _ = pergNum Nao
+
+not' :: Pergunta -> Pergunta -> Int
+not' Sim Sim = pergNum Nao
+not' Nao Nao = pergNum Sim
+not' _ _ = pergNum Sim
+
+
+data Graus = Celsius | Fahrenheit | Kelvin deriving Show
+data Conversao = Conversao {Valor :: Double, nome :: String} deriving Show
+
+converterCelsius :: Double -> Graus -> Double || String
+converterCelsius x Fahrenheit = (x - 32) * 5/9 
+converterCelsius x Kelvin = x + 273.1
+converterCelsius x Celsius = x
+converterCelsius _ _ = "Não pode inserir String, caso tenha inserido valor deve escolher entre Kelvin ou Farenheit" 
+
+converterKelvin :: Double -> Graus -> Double || String
+converterKelvin x Fahrenheit = ((x - 32) * 5/9) + 273.15 
+converterKelvin x Celsius = x + 273.1
+converterKelvin x Kelvin = x
+converterKelvin _ _ = "Não pode inserir String, caso tenha inserido valor deve escolher entre Celsius ou Farenheit"
+
+converterFahrenheit :: Double -> Graus -> Double || String
+converterFahrenheit x Celsius = (x * 9/5) + 32 
+converterFahrenheit x Kelvin = (x - 273.1) * 9/5 + 32
+converterFahrenheit x Fahrenheit = x
+converterFahrenheit _ _ = "Não pode inserir String, caso tenha inserido valor deve escolher entre Kelvin ou Celsius"
